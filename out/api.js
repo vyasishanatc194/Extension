c4056e3f5d4907d3b2ac4e07f9b796f16581e54b
kcitrusbug <krishna.citrusbug@gmail.com>
2018-10-09 19:44:23 +0530

    data push

---
+++ out/api.js
@@ -0,0 +1,10 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "friendsDetails"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
